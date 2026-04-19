22dd07c59ad423f93fd80f9aa145e97e01846b48
Micaela Lugo <lugomicaela90@gmail.com>
2026-04-19 02:03:59 -0300

    AHORA SÍ

--- ej1.c
+++ ej1.c
@@ -1,1 +1,22 @@
 ../azcurra/ej1.c
+#include <stdio.h>
+
+/*Hallar la superficie de un triángulo conociendo la base y la altura. Solicitarle los
+datos de entrada al usuario.*/
+
+int main()
+{
+        int base;
+        int altura;
+        float superficie;
+
+        printf("Introduzca la base de su triángulo: ");
+        scanf("%i", &base);
+
+        printf("Ahora ingrese la altura: ");
+        scanf("%i", &altura);
+
+        superficie = (base * altura) / 2.0;
+        printf("La superficie de su triángulo es: %f\n", superficie);
+        return 0;
+}
